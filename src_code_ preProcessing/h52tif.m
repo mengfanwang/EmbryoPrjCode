0228a1abc63160fc23157ea5c22b52ea0e81cc17
Mengfan Wang <mengfanw@vt.edu>
2022-08-30 14:40:43 -0400

    routine

--- src_code_ preProcessing/h52tif.m
+++ src_code_ preProcessing/h52tif.m
@@ -6,6 +6,7 @@ if isunix
     path_name = '/work/Mengfan/Embryo/22-01-11';
     source_data = 'myf5GFP-H2BmCherry.v1.h5';
     target_folder = 'data';
+    mode = 'merge';  % if mode == merge, combine two views together
 else
     path_name = 'E:\Embryo\TM0-49';
     source_data = 'H2BGFP_TM0-49.h5';
@@ -16,22 +17,40 @@ end
 [h5_struct, num_view, name_view] = readh5info(fullfile(path_name, source_data));
 
 % read h5 data and save as tif file
+if strcmp(mode, 'merge')
+    target_folder = [target_folder, '_merge'];
+end
 if ~isfolder(fullfile(path_name, target_folder))
     mkdir(fullfile(path_name, target_folder));
 end
 num_time = length(h5_struct);
-for tt = 1:num_time
+for tt = 250:250 %1:num_time
     fprintf('Processing time point %d/%d:', tt, num_time);
     tt_ind = num2str(99999+tt);
     tt_ind = tt_ind(2:6);
     mkdir(fullfile(path_name, target_folder, tt_ind));
-    for vv = 1:num_view
-        fprintf(' %d', vv);
-        vv_ind = name_view{vv};
-        data = hdf5read(fullfile(path_name, source_data),['/t' tt_ind '/s' vv_ind '/0/cells']);
-        tifwrite(uint16(data),fullfile(path_name, target_folder,  tt_ind, vv_ind));
+    if ~strcmp(mode, 'merge')
+        for vv = 1:num_view
+            fprintf(' %d', vv);
+            vv_ind = name_view{vv};
+            data = hdf5read(fullfile(path_name, source_data),['/t' tt_ind '/s' vv_ind '/0/cells']);
+            tifwrite(uint16(data),fullfile(path_name, target_folder,  tt_ind, vv_ind));
+        end
+        fprintf('\n');
+    else
+        for vv = 1:num_view/2
+            fprintf(' %d', vv);
+            vv_ind = name_view{vv+4};
+            data2 = hdf5read(fullfile(path_name, source_data),['/t' tt_ind '/s' vv_ind '/0/cells']);
+            vv_ind = name_view{vv};
+            data = hdf5read(fullfile(path_name, source_data),['/t' tt_ind '/s' vv_ind '/0/cells']);
+            weight_1 = repmat([1:-1/1919:0]',1,1920);
+            weight_2 = repmat([0:1/1919:1]',1,1920);
+            data = (double(data).*weight_1 + double(data2).*weight_2)/2;
+            tifwrite(uint16(data),fullfile(path_name, target_folder,  tt_ind, vv_ind));
+        end
+        fprintf('\n');
     end
-    fprintf('\n');
 end
 
 
